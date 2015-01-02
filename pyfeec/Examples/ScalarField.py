7c813060c4cf22cdf971a277298c98e2d66819bb
aeftimia <alexeftimiades@gmail.com>
2015-01-01 22:14:51 -0500

    first commit

---
+++ pyfeec/Examples/ScalarField.py
@@ -0,0 +1,44 @@
+import kahler
+from numpy import linspace, asarray, identity, zeros, pi
+from numpy.random import rand
+from scipy.linalg import eig
+from matplotlib.pylab import show
+
+N = 20
+dim = 2
+
+shape = [N]*dim
+
+grid_indices = kahler.grid_indices(shape)
+simplices = kahler.grid(grid_indices)
+coordinates = [linspace(0,pi,s) for s in shape]
+vertices = kahler.embed(grid_indices, coordinates)
+sc = kahler.SimplicialComplex(simplices, vertices)
+
+interior = sc[0].interior
+#interior = range(sc[0].num_simplices) #uncomment for Nuemann conditions
+K = -sc[0].laplace_beltrami[interior, :][:, interior]
+
+val0, vec0 = eig(K.todense())
+vec0 = vec0.T
+
+values = [(val.real, vec) for val, vec in zip(val0, vec0) if val.real > .9 and abs(val.imag) < 0.1]
+values.sort(key=lambda x: x[0])
+eigenvalues = []
+eigenvectors = []
+for i,value in enumerate(values):
+    eigenvalues.append(value[0])
+    eigenvectors.append(value[1])
+eigenvalues = asarray(eigenvalues)
+eigenvectors = asarray(eigenvectors)
+
+
+n = 2
+
+barycenters = sc[-1].points.mean(1).real
+z = zeros(sc[0].num_simplices)
+z[interior] = eigenvectors[n].real
+
+kahler.scalar_field2d(barycenters, sc[0].sharpen(z).real, 1000)
+
+show()
